2eef3349ce626a12401a1f9328ae534e0e8f04e4
MRWilliamsGit <87984691+MRWilliamsGit@users.noreply.github.com>
2022-06-28 14:12:15 -0400

    updated searchterm

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from scripts.API_tools import access_API, API_request
 from scripts.getsummary import getmodel, summarize
 
 if __name__ == "__main__":
+
+    searchterm = "NVDA"
     
     headers = access_API()
-    df, block = API_request(headers, "NVDA")
+    df, block = API_request(headers, searchterm)
     model, t = getmodel()
     output = summarize(model, t, block)
 
