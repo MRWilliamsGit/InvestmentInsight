f4a65c922e9cead30172a8bb74cd37d4e10ab142
MRWilliamsGit <87984691+MRWilliamsGit@users.noreply.github.com>
2022-07-08 00:18:57 -0400

    pending spinner added

--- main.py
+++ main.py
@@ -8,10 +8,6 @@ import streamlit as st
 def main():
 
     st.title("Stock Support")
-
-    which = st.radio("",("Generative Summary","Extractive Summary"),)
-    st.write("<style>div.row-widget.stRadio > div{flex-direction:row;}</style>",unsafe_allow_html=True,)
-
     searchterm = st.text_input("Enter a stock name. Ex: AAPL, NVDA, AMZN", ' ')
 
     if searchterm != ' ':
@@ -21,22 +17,22 @@ def main():
         #print(len(df))
         st.write("Number of news posts found: ", len(df))
 
-        if which == "Generative Summary":
+        with st.spinner('Generating Summary'):
             # generative summarization
             # length = the length of summary to return for each text chunk
             text_list = make_cloud_chunks(df)
             gfs = GenFinSummarizer()
             output = gfs.summarize(text_list, length=400)
-            #print(output)
-            st.write(output)
-        else:
-            # extractive summarization
-            # top_n = the number of sentences to extract
-            block = makecloud(df)
-            efs = ExFinSummarizer()
-            output = efs.summarize(block, top_n=5)
-            #print(output)
-            st.write(output)
+        #print(output)
+        st.write(output)
+
+        # extractive summarization
+        # top_n = the number of sentences to extract
+        #block = makecloud(df)
+        #efs = ExFinSummarizer()
+        #output = efs.summarize(block, top_n=5)
+        #print(output)
+        #st.write(output)
 
 if __name__ == "__main__":
     main()
